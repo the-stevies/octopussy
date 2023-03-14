56a5d9d745240981115bab9db3afa8481151d081
stefan <post@paperjazz.eu>
2023-03-14 17:12:02 +0100

    cards

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
 import { useState, useEffect } from 'react'
-import './App.css';
+import './styles/App.css';
+import background from "./img/filz.jpg";
 import SingleCard from './components/SingleCard';
+
 
 const $ = q => document.querySelector(q)
 const ipath = process.env.PUBLIC_URL
 
+const cardImages = [
+  { "src": '/img/fischli_06.jpg', matched: false },
+  { "src": "/img/fischli_05.jpg", matched: false },
+  { "src": "/img/fischli_04.jpg", matched: false },
+  { "src": "/img/fischli_01.jpg", matched: false },
+  { "src": "/img/fischli_02.jpg", matched: false },
+  { "src": "/img/fischli_03.jpg", matched: false }
+]
+
+
+
 function App() {
+  const [cards, setCards] = useState([])
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map(card => ({ ...card, id: Math.random() }))
+    setCards(shuffledCards)
+  }
+
+
+
   return (
-    <div className="App">
-      <SingleCard />
-      
+    <div className="App" style={{ backgroundImage: `url(${background})` }}>
+      <button onClick={shuffleCards}>New Game</button>
+      <div className="card-grid">
+      {cards.map(card => (
+        <SingleCard
+          key={card.id}
+          card={card}
+        />
+      ))}
+      </div>
+
+
     </div>
   );
 }
